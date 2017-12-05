3bdfc82c4e7b4c5aa0fe3bc90a432efcde993196
André Tonic <andre_tonic@iMac-de-Andre-Tonic.local>
2017-12-05 16:37:06 +0100

    Initial Commit

---
+++ Calculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Calculator
+//
+//  Created by André Tonic on 05/12/2017.
+//  Copyright © 2017 André Tonic. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
