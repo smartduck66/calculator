b6b8ca6cfacdad7470a649e964309e9152434cbf
André Tonic <andre_tonic@imac-de-andre-tonic.local>
2018-01-03 17:05:39 +0100

    3 janvier 2018 - Exos page 253

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -3,7 +3,7 @@
 // "Programming -- Principles and Practice Using C++" de Bjarne Stroustrup (2ème édition : 2014)
 // Commit initial : 10 décembre 2017
 // Commit en cours : 3 janvier 2018 - Exos page 253
-// Caractères spéciaux : [ ]   '\n'   {  }   ||
+// Caractères spéciaux : [ ]   '\n'   {  }   ||   ~
 
 // Chemin pour VSCODE
 // #include "../../std_lib_facilities.h"
@@ -12,25 +12,44 @@
 #include "std_lib_facilities.h"
 
 // Définition de constantes symboliques pour clarifier le code
-// -----------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------------------------
 const char number='8';                  // "a value" dans la classe Token
+
 const char quit='q';                    // Quitter
-const string declexit="exit";           // Exit Keyword, identique dans son comportement à "Quitter"
+const string exitkey="exit";            // Exit Keyword, identique dans son comportement à "Quitter"
+
 const char print=';';                   // "Imprimer" le résultat
+
 const string prompt="> ";               // Demande d'une saisie
 const string result="= ";               // Indique que ce qui suit est un résultat
-const char name='a';                    // name token
+
+const char name='a';                    // name : token
+
 const char let='L';                     // déclaration/initialisation d'une variable : token
 const string declkey="let";             // déclaration/initialisation d'une variable : keyword
+
 const char const_let='C';               // déclaration/initialisation d'une constante : token
 const string const_letkey="const_let";  // déclaration/initialisation d'une constante : keyword
-const char assign='S';                  // assignation var token (modification de la valeur d'une variable déjà créée par let)
-const string assignkey="set";           // assignation var keyword
+
+const char assign='S';                  // assignation d'une variable : token (modification de la valeur d'une variable déjà créée par let)
+const string assignkey="set";           // assignation d'une variable : keyword
+
+const char list_var='v';                // Lister les variables à l'écran : token
+const string list_varkey="lv";          // Lister les variables à l'écran : keyword
+
+const char del_var='d';                 // Supprimer la table de variables : token
+const string del_varkey="delete";       // Supprimer la table de variables : keyword
+
 const char racine='R';                  // racine carrée token
 const string sqrtkey="sqrt";            // racine carrée keyword
+
 const char puissance='P';               // puissance token
 const string puissancekey="pow";        // puissance keyword
 
+const char help='H';                    // Imprimer l'aide de la calculette : token
+const string helpkey="help";            // Imprimer l'aide de la calculette : keyword
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
 class Variable {            // Cette classe sert de socle à la gestion des variables & constantes dans la calculette (ex : let x=2; ou set x=4;)
 public:
     string name;
@@ -42,14 +61,38 @@ public:
     
 };
 
+class Symbol_table {        // Cette classe encapsule la classe Variable ainsi que ses fonctions associées (exo 4, p 253)
+public:                     // Question : aucun constructeur défini implicitement. Problème ?
+    // fonctions-membre
+    double get_value(string s);
+    double set_value(string s, double d);
+    bool is_declared(string var);
+    double define_name(string var,double val, bool isConstante);
+    void print_value();
+    void effacer();
+    
+    // Destructeur
+    // ~Symbol_table();
+    
+    // Constructeur
+    // Symbol_table(vector<Variable>vt) :var_table(vt) { }
+
+private:
+    //Seules les fonctions-membre peuvent manipuler les données
+    vector<Variable>var_table;
+
+};
+
 // ------------------------------------------------------------------------------------
-// Définition des 2 fonctions-membre de la classe Variable
+// Définition des fonctions-membre de la classe Variable
 // ------------------------------------------------------------------------------------
 
-vector<Variable>var_table;
+void Symbol_table::effacer(){   // On efface toutes les valeurs du tableau de variables - Utilisation de la fonction clear() de la librairie standard
+    var_table.clear();
+}
 
 // The get_value() member function returns the value of the Variable named s
-double get_value(string s){
+double Symbol_table::get_value(string s){
     for (const Variable& v:var_table)
         if (v.name==s) return v.value;
     
@@ -58,7 +101,7 @@ double get_value(string s){
 }
 
 // The set_value() member function sets the Variable named s to d
-double set_value(string s, double d){
+double Symbol_table::set_value(string s, double d){
     for (Variable& v:var_table)
         if (v.name==s){
             if (v.constante)error("set: cette variable est une constante et ne peut être modifiée : ",s);
@@ -71,9 +114,44 @@ double set_value(string s, double d){
     return 1;
 }
 
+bool Symbol_table::is_declared(string var)   // is var already in var_table ?
+{
+    for (const Variable& v:var_table)
+        if (v.name==var) return true;
+    return false;
+    
+}
 
+double Symbol_table::define_name(string var,double val, bool isConstante)   // add (var,val, constante) to var_table
+{
+    if (is_declared(var)) error(var," declared twice");
+    
+    if (isConstante)
+        var_table.push_back(Variable{var,val, true});   // On initialise une constante
+    else
+        var_table.push_back(Variable{var,val, false});  // On initialise une variable modifiable par un éventuel set
+    
+    return val;
+    
+}
 
-//----------------------------------------------------------------------------------
+void Symbol_table::print_value()   // Imprime à l'écran la liste des variables déclarées
+{
+    // Aucune valeur ?
+    if (var_table.size()==0)error("set: aucune variable n'a encore été déclarée : ");
+    
+    cout<<"Nombre de variables déclarées : "<<var_table.size()<<"\n";
+    for (Variable& v:var_table)
+        cout << "Variable "<<v.name<<" = "<<v.value<<"\n";
+    
+}
+
+//--------------------------------------------------------------------------------------------
+
+Symbol_table sym; // Instanciation de la classe Symbol_table : fourniture des fonctions-membre
+
+//--------------------------------------------------------------------------------------------
+
 
 class Token {               // Cette classe sert de socle à la gestion des "Tokens"
 public:
@@ -182,11 +260,20 @@ Token Token_stream::get()
                     return Token{const_let};    //déclaration initiale d'une constante
                 if (s==assignkey)
                     return Token{assign};       //assignation d'une variable (=modification) après sa déclaration initiale par un let
-                else if (s==sqrtkey)
+                if (s==list_varkey)
+                    return Token{list_var};     //Impression de la liste des variables
+                if (s==del_varkey)
+                    return Token{del_var};     //Suppression de la liste des variables
+                
+                if (s==helpkey)
+                    return Token{help};         //Aide
+                
+                if (s==sqrtkey)
                     return Token{racine};       //racine carrée keyword
-                else if (s==puissancekey)
+                if (s==puissancekey)
                     return Token{puissance};    //puissance keyword
-                else if (s==declexit)
+                
+                if (s==exitkey)
                     return Token{quit};         //Même comportement que le "case quit" plus haut
                 
                 return Token{name,s};
@@ -237,14 +324,14 @@ double primary()
             return primary();
             
         case number:            // we use '8' to represent a number
-            return t.value;  // return the number's value
+            return t.value;     // return the number's value
             
         case name:
-            return get_value(t.name);
+            return sym.get_value(t.name);
         
         case racine:        // Traitement de la racine carrée
         {
-            double d = expression();    // On attend une expression (ex : 4 ou 4-2)
+            double d = term();  // On attend une expression (ex : 4 ou 4-2)
             if (d<0) error("Une racine carrée ne peut s'appliquer sur un nombre négatif !");
             return sqrt(d);
         }
@@ -358,29 +445,6 @@ void clean_up_mess()
     ts.ignore(print);
 }
 
-//------------------------------------------------------------------------------
-bool is_declared(string var)   // is var already in var_table ?
-{
-    for (const Variable& v:var_table)
-        if (v.name==var) return true;
-    return false;
-    
-}
-
-//------------------------------------------------------------------------------
-double define_name(string var,double val, bool isConstante)   // add (var,val, constante) to var_table
-{
-    if (is_declared(var)) error(var," declared twice");
-    
-    if (isConstante)
-        var_table.push_back(Variable{var,val, true});   // On initialise une constante
-    else
-        var_table.push_back(Variable{var,val, false});  // On initialise une variable modifiable par un éventuel set
-    
-    return val;
-    
-}
-
 
 //------------------------------------------------------------------------------
 double declaration(bool isConstante)        // Déclaration d'une variable (ex : let x=2;)
@@ -396,7 +460,7 @@ double declaration(bool isConstante)        // Déclaration d'une variable (ex :
     if (t2.kind!='=') error("= missing in declaration of ",var_name);
     
     double d=expression();
-    define_name(var_name,d, isConstante);
+    sym.define_name(var_name,d, isConstante);
     return d;
     
 }
@@ -415,7 +479,7 @@ double assignation()        // Assignation/modification d'une variable (ex : set
     if (t2.kind!='=') error("= missing in assignation of ",var_name);
     
     double d=expression();
-    set_value(var_name,d);  // L'appel à "set_value" est fait directement sans créer un "define_name" qui vérifie la présence de la variable :
+    sym.set_value(var_name,d);  // L'appel à "set_value" est fait directement sans créer un "define_name" qui vérifie la présence de la variable :
                             // en effet, ce test est déjà fait dans la fonction set_value
     return d;
     
@@ -435,6 +499,26 @@ double statement()    // handle expressions and declarations
         case assign:                // Assignation/Modification d'une variable déjà créée
             return assignation();
             
+        case list_var:              // Impression de la liste des variables
+            sym.print_value();
+            return 0;
+        
+        case help:              // Impression de l'aide
+        {
+            cout << "Aide de la calculette\n";
+            cout << "- opérations courantes : *, -, /, +, %\n";
+            cout << "- let, set, const_let, lv, delete pour les variables/constantes\n";
+            cout << "- factorielle : !  puissance : pow(x,y)  racine carrée : sqrt(x)\n";
+            cout << "----------------------------------------------------------------\n";
+            return 0;
+        }
+            
+        case del_var:              // Suppression de la liste des variables
+            {
+            sym.effacer();
+            return 0;
+            }
+            
         default:
             ts.putback(t);
             return expression();
@@ -453,11 +537,6 @@ void calculate()    // expression evaluation loop
         ts.putback(t);
         cout << result << statement() << '\n';
         
-        // Debug : vérification des variables déclarées
-        // for (Variable& v:var_table)
-        //    cout << "Variable "<<v.name<<" = "<<v.value<<"\n";
-        
-        
     }
     catch (exception& e) {
         cerr << "error: " << e.what() << '\n';
@@ -471,15 +550,11 @@ int main()
 try
 {
     //predefined names:
-    define_name("pi",3.1415926535, true);
-    define_name("e",2.7182818284, true);
-    define_name("k",1000, true);
-    
-    cout << "Saisis une expression suivie d'un ; (q ou exit pour quitter) :\n";
-    cout << "- opérations courantes : *, -, /, +, %\n";
-    cout << "- let, set, const_let pour les variables/constantes\n";
-    cout << "- factorielle : !  puissance : pow(x,y)  racine carrée : sqrt(x)\n";
-    cout << "----------------------------------------------------------------\n";
+    sym.define_name("pi",3.1415926535, true);
+    sym.define_name("e",2.7182818284, true);
+    sym.define_name("k",1000, true);
+
+    cout << "Saisis une expression suivie d'un ; (q ou exit pour quitter, help pour l'aide) :\n";
     
     calculate();
     // keep_window_open(); Uniquement pour Windows
